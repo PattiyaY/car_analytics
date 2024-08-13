8997420a61f0ceabfd7ed359b83e912db098ae1d
Jan Poonthong <janpoonthong628@gmail.com>
2024-08-13 20:47:07 +0700

    feat: create /dashboard and /highlight routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,22 @@
 import { StrictMode } from "react";
-import Dashboard from "./App.jsx";
+import Dashboard from "./Dashboard.jsx";
+import Highlight from "./Highlight.jsx";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 const router = createBrowserRouter([
+  // Default path: "/"
   {
-    path: "/",
+    path: "/dashboard",
     element: <Dashboard />,
   },
-]);
+  {
+    path: "/highlight",
+    element: <Highlight />,
+  },
 
+]);
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
