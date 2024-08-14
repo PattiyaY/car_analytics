d155d8514f0aefdc7243e1ddffa1d7eb43ef1747
PattiyaY <pattiya.2547@icloud.com>
2024-08-14 08:51:54 +0700

    refactor: remove <button> tag and move class name to <Link>

--- src/Home.jsx
+++ src/Home.jsx
@@ -15,16 +15,18 @@ function Home() {
           </p>
 
           {/* Options */}
-          <div className="flex flex-col sm:flex-row justify-center gap-4 sm:gap-8 mt-6 sm:mt-10 text-gray-500">
-            <Link to="/dashboard">
-              <button className="px-6 py-3 sm:px-10 sm:py-4 bg-white rounded hover:text-black hover:shadow-lg hover:shadow-cyan-500/50">
-                Dashboard
-              </button>
+          <div className="flex flex-col sm:flex-row justify-center items-center gap-4 sm:gap-8 mt-6 sm:mt-10 text-gray-500">
+            <Link
+              to="/dashboard"
+              className="px-6 py-3 sm:px-10 sm:py-4 bg-white rounded hover:text-black hover:shadow-lg hover:shadow-cyan-500/50"
+            >
+              Dashboard
             </Link>
-            <Link to="/highlight">
-              <button className="px-6 py-3 sm:px-10 sm:py-4 bg-white rounded hover:text-black hover:shadow-lg hover:shadow-cyan-500/50">
-                Highlight
-              </button>
+            <Link
+              to="/highlight"
+              className="px-6 py-3 sm:px-10 sm:py-4 bg-white rounded hover:text-black hover:shadow-lg hover:shadow-cyan-500/50"
+            >
+              Highlight
             </Link>
           </div>
         </div>
