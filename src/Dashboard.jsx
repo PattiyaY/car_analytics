ebdaf54fa078cb56d398002c55dcec2a5b85fdfd
PattiyaY <pattiya.2547@icloud.com>
2024-08-18 21:33:18 +0700

    feat: create draft pie chart and bar stack

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -83,51 +83,51 @@ function Table() {
   console.log(data);
 
   return (
-    <div className="flex my-5 text-white">
-      <div className="relative rounded-xl overflow-auto">
-        <div className="shadow-sm overflow-hidden my-4">
-          {Object.keys(data).map((carModel) => (
-            <div key={carModel} className="p-1 w-[350px]">
-              <details className="bg-slate-800 rounded px-5">
-                <summary>{carModel}</summary>
-                <table
-                  className="border-collapse table-auto w-full text-sm mb-6"
-                  key={carModel}
-                >
-                  <thead>
-                    <tr>
-                      <th className="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
-                        #
-                      </th>
-                      <th className="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
-                        Model
-                      </th>
-                      <th className="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
-                        Price(THB)
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="bg-white">
-                    {data[carModel].map((car, index) => (
-                      <tr key={`${carModel}-${index}`}>
-                        <td className="border-b border-slate-100 dark:border-slate-700 p-3 text-slate-500 dark:text-black">
-                          {index + 1}
-                        </td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-black">
-                          {car["Model"]}
-                        </td>
-                        <td className="border-b border-slate-100 dark:border-slate-700 p-2 pr-8 text-slate-500 dark:text-black">
-                          {car["Prc"]}
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </details>
-            </div>
-          ))}
-        </div>
+    <div className="flex flex-col my-5 text-white">
+      <div className="flex flex-row justify-between">
+        <div className="mx-2 bg-black w-1/2">pie chart</div>
+        <div className="mx-2 bg-black w-1/2">bar stack</div>
       </div>
+      {Object.keys(data).map((carModel) => (
+        <div key={carModel} className="p-1 w-full">
+          <details className="bg-black rounded px-5">
+            <summary className="text-xl">{carModel}</summary>
+            <table
+              className="border-collapse table-auto w-full text-sm mb-6"
+              key={carModel}
+            >
+              <thead>
+                <tr>
+                  <th className="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                    #
+                  </th>
+                  <th className="border-b dark:border-slate-600 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                    Model
+                  </th>
+                  <th className="border-b dark:border-slate-600 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-200 text-left">
+                    Price(THB)
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white">
+                {data[carModel].map((car, index) => (
+                  <tr key={`${carModel}-${index}`}>
+                    <td className="border-b border-slate-100 dark:border-slate-700 p-3 text-slate-500 dark:text-black">
+                      {index + 1}
+                    </td>
+                    <td className="border-b border-slate-100 dark:border-slate-700 p-2 text-slate-500 dark:text-black">
+                      {car["Model"]}
+                    </td>
+                    <td className="border-b border-slate-100 dark:border-slate-700 p-2 pr-8 text-slate-500 dark:text-black">
+                      {car["Prc"]}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </details>
+        </div>
+      ))}
     </div>
   );
 }
