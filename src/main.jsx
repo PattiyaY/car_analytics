7c07952ed85d0e9be1fdef3679cc314c6e8e5c91
Jan Poonthong <janpoonthong628@gmail.com>
2024-08-19 13:02:02 +0700

    build: use createHashRouter for deploying on GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import { StrictMode } from "react";
-import Home from "./Home.jsx";
 import Dashboard from "./Dashboard.jsx";
 import Highlight from "./Highlight.jsx";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./error-page";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Dashboard />,
