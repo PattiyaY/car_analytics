37420cdbc0f24b4a2c9a85a9f7046728b7e61340
PattiyaY <pattiya.2547@icloud.com>
2024-08-19 14:11:06 +0700

    feat: add data into pie chart

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -45,46 +45,77 @@ function Table() {
     setBrandToCarsMap(groupedCars);
   }
 
+  function pieChartData(brandToCarsMap) {
+    const data = [];
+    Object.keys(brandToCarsMap).forEach((carModel, index) => {
+      const carCount = brandToCarsMap[carModel].length;
+      data.push({
+        id: index + 1,
+        value: carCount,
+        label: carModel,
+      });
+    });
+
+    data.sort((a, b) => b.value - a.value);
+    return data;
+  }
+
+  const colors = [
+    "#FAD02E", // Pastel Yellow
+    "#F28D35", // Pastel Orange
+    "#F77F7D", // Pastel Pink
+    "#D7A9A3", // Pastel Red
+    "#A5D8DD", // Pastel Blue
+    "#B5E2A0", // Pastel Green
+    "#E6B0AA", // Pastel Rose
+  ];
+
   useState(() => {
     groupCarsByBrand();
   }, []);
 
   return (
     <div className="flex flex-col text-white bg-white">
-      <div className="flex flex-row justify-between text-neutral-100 mt-20">
-        <div className="mx-2 w-1/2">
-          <PieChart
-            series={[
-              {
-                data: [
-                  { id: 0, value: 10, label: "series A" },
-                  { id: 1, value: 15, label: "series B" },
-                  { id: 2, value: 20, label: "series C" },
-                ],
-              },
-            ]}
-            width={500}
-            height={350}
-          />
-        </div>
-        <div className="mx-2 w-1/2">
-          <BarChart
-            xAxis={[
-              {
-                id: "barCategories",
-                data: ["bar A", "bar B", "bar C"],
-                scaleType: "band",
-              },
-            ]}
-            series={[
-              {
-                data: [2, 5, 3],
-              },
-            ]}
-            width={500}
-            height={350}
-          />
-        </div>
+      <h1 className="text-center text-4xl p-4 mb-10 font-semibold bg-[#3C3D37] rounded">
+        Car Brand Proportions
+      </h1>
+      <div className="w-1/2">
+        <PieChart
+          colors={colors}
+          series={[
+            {
+              arcLabel: (item) => `(${item.value})`,
+              innerRadius: 10,
+              paddingAngle: 1,
+              startAngle: 90,
+              endAngle: 500,
+              data: pieChartData(brandToCarsMap),
+            },
+          ]}
+          width={1200}
+          height={400}
+        />
+      </div>
+      <h1 className="text-center text-4xl p-4 my-10 font-semibold bg-[#3C3D37] rounded">
+        Car Models by Brand
+      </h1>
+      <div className="mx-2 w-1/2">
+        <BarChart
+          xAxis={[
+            {
+              id: "barCategories",
+              data: ["bar A", "bar B", "bar C"],
+              scaleType: "band",
+            },
+          ]}
+          series={[
+            {
+              data: [2, 5, 3],
+            },
+          ]}
+          width={1200}
+          height={400}
+        />
       </div>
       <div className="rounded-t-[100px] mt-20 bg-[#3C3D37] pl-2 py-3">
         <p className="text-center text-2xl mt-5 mb-10 font-semibold">
@@ -93,7 +124,9 @@ function Table() {
         {Object.keys(brandToCarsMap).map((carModel) => (
           <div key={carModel} className="p-1 w-full border-slate-400 rounded">
             <details className="rounded px-5">
-              <summary className="text-xl">{carModel}</summary>
+              <summary className="text-xl text-stone-400 hover:text-white">
+                {carModel}
+              </summary>
               <table
                 className="border-collapse table-auto w-full text-sm mb-6 border-slate-400 mt-5"
                 key={carModel}
