0dabf19e2269e70bc9a59817e7be81ada2e43440
Jan Poonthong <janpoonthong628@gmail.com>
2024-08-20 18:40:51 +0700

    feat: add active page link styling

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,27 +1,33 @@
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 
 function NavBar() {
+  const links = ["", "highlight"];
   return (
     <nav className="px-16 py-5">
       <div className="flex justify-between">
         <a href="/" className="">
           <span className="font-medium text-2xl">Car Analytics</span>
         </a>
-        <div className="">
+        <div className="navbar">
           <ul className="flex gap-5">
-            <li>
-              <Link to="/" className="hover:underline hover:text-blue-500">
-                Dashboard
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/highlight"
-                className="hover:underline hover:text-blue-500"
+            {links.map((link) => (
+              <NavLink
+                key={link}
+                to={`/${link}`}
+                className={({ isActive, isPending }) =>
+                  isActive ? "active" : isPending ? "pending" : ""
+                }
               >
-                Highlight
-              </Link>
-            </li>
+                <li>
+                  <Link
+                    to={`/${link}`}
+                    className="hover:underline hover:text-blue-500"
+                  >
+                    {link === "" ? "Dashboard" : "Highlight"}
+                  </Link>
+                </li>
+              </NavLink>
+            ))}
           </ul>
         </div>
       </div>
